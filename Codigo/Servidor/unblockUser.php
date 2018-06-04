9575190c3f72a99c152e82991c4df99cc2eb8d01
Danny Xie Li <dnnxl15@gmail.com>
2018-06-03 18:33:50 -0600

    Block y unblock user servidor

---
+++ Codigo/Servidor/unblockUser.php
@@ -0,0 +1,25 @@
+<?php
+	
+	// Connect to the database
+	$con = mysqli_connect("localhost:3306","root","","cinetec");
+    if (mysqli_connect_errno())
+    {
+       echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    // Parameters
+    $username = $_GET["username"];
+
+
+	// Call the store procedure
+	$sql= "CALL unblockUser($username)";
+
+	$call = mysqli_prepare($con ,'CALL unblockUser(?)');
+	mysqli_stmt_bind_param($call, 's', $username);
+	mysqli_stmt_execute($call);
+	//$result = mysqli_query($con ,$sql);
+	
+	
+	header('Content-Type:Application/json');
+    mysqli_close($con); // Close connection
+ ?>
